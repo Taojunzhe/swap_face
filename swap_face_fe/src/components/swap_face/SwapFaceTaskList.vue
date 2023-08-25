ee48f7fa7a5874476735da9da2ef4d0f365c2141
taojunzhe.9507 <taojunzhe.9507@BYTEDANCE.COM>
2023-08-25 11:19:50 +0800

    update

--- swap_face_fe/src/components/swap_face/SwapFaceTaskList.vue
+++ swap_face_fe/src/components/swap_face/SwapFaceTaskList.vue
@@ -8,6 +8,7 @@
       </template>
     </el-table-column>
     <el-table-column prop="creator" label="创建人" width="180" />
+    <el-table-column prop="ctime" label="创建时间" width="180"></el-table-column>
     <el-table-column label="操作" width="180">
       <template #default="props">
         {{ props.row["taskResultMap"]["resultImageName"] }}
