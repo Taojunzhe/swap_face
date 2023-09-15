7acf2784ca77a77406ba461bd220cc30281b94e4
taojunzhe.9507 <taojunzhe.9507@BYTEDANCE.COM>
2023-09-15 10:38:55 +0800

    update

--- swap_face_fe/src/components/swap_face/TaskCreator.vue
+++ swap_face_fe/src/components/swap_face/TaskCreator.vue
@@ -1,10 +1,11 @@
 <template>
   <el-tabs v-model="activeName" tabPosition="top">
+    <el-tab-pane label="MD主题换脸" name="midjourney"
+      ><mid-journey-swap-face @closeDialog="$emit('closeDialog')"
+    /></el-tab-pane>
     <el-tab-pane label="主题换脸" name="swap_face"
       ><single-swap-face
     /></el-tab-pane>
-    <el-tab-pane label="MD主题换脸" name="midjourney"
-      ><mid-journey-swap-face @closeDialog="$emit('closeDialog')"/></el-tab-pane>
   </el-tabs>
 </template>
 
@@ -13,4 +14,6 @@ import { ref } from "vue";
 import type { TabsPaneContext } from "element-plus";
 import SinglgSwapFace from "./form/SingleSwapFace.vue";
 import MidJourneySwapFace from "./form/MidJourneySwapFace.vue";
+
+const activeName = ref("midjourney");
 </script>
