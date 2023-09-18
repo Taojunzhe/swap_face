05120a062947601a541852e2d52db23c8210e419
taojunzhe.9507 <taojunzhe.9507@BYTEDANCE.COM>
2023-09-18 13:38:29 +0800

    update

--- swap_face_fe/src/components/swap_face/form/SingleSwapFace.vue
+++ swap_face_fe/src/components/swap_face/form/SingleSwapFace.vue
@@ -70,13 +70,6 @@ import { genFileId } from "element-plus";
 import type { UploadInstance, UploadProps, UploadRawFile } from "element-plus";
 import axios from "axios";
 
-const props = defineProps({
-  closeDialog: {
-    type: Function,
-    required: true,
-  },
-});
-
 const upload = ref<UploadInstance>();
 
 const taskTypeOptions = [
