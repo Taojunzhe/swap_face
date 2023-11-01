690abb3b4f11a873e277ce1e02511fb3f3561fc4
taojunzhe.9507 <taojunzhe.9507@BYTEDANCE.COM>
2023-11-01 10:50:21 +0800

    url

--- swap_face_fe/config/index.js
+++ swap_face_fe/config/index.js
@@ -3,9 +3,9 @@ export default {
     baseUrl: 'http://127.0.0.1:9000/api/v1' // 测试接口域名
   },
   beta: {
-    baseUrl: 'http://127.0.0.1:9000/api/v1' // 测试接口域名
+    baseUrl: 'http://81.68.187.103/api/v1' // 测试接口域名
   },
   release: {
-    baseUrl: 'http://127.0.0.1:9000/api/v1' // 正式接口域名
+    baseUrl: 'http://81.68.187.103/api/v1' // 正式接口域名
   }
 }
